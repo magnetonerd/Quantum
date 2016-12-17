f70238c83a4d84c4de236eee35aab6aedb19acd6
magnetonerd <magnetonerd@gmail.com>
2016-12-17 00:41:42 -0600

    Update hermitepoly.py

--- hermitepoly.py
+++ hermitepoly.py
@@ -1,7 +1,11 @@
+"""
+Calculating the nth Hermite Polynomial H(x) using
+the recursive definition of Hermite Polynomials. 
+"""
 def H(n,x):
-    H0 = 1
-    H1 = 2*x
+    H0 = 1 #Initializing H_0
+    H1 = 2*x #Initializing H_1
     for k in range(1,n):
-        H0,H1 = H1,2*x*H1-2*k*H0
+        H0,H1 = H1,2*x*H1-2*k*H0 #Recursive call of definition
 
-    return H1
+    return H1 #The nth Hermite Polynomial
