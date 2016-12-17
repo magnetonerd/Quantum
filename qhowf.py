ce9ca4ea6d6c305cb69bf4472fb8945193a16b76
magnetonerd <magnetonerd@gmail.com>
2016-12-17 00:45:30 -0600

    Update qhowf.py

--- qhowf.py
+++ qhowf.py
@@ -2,6 +2,13 @@ from numpy import *
 from math import factorial
 from hermitepoly import *
 
+"""
+The wavefunction of the quantum harmonic oscillator:
+          1
+____________________  exp(-x^2 /2) H_n (x)
+sqrt(2^n n! sqrt(n))
+"""
+
 def psi(n,x):
-    const = 1/sqrt(2**n*factorial(n)*sqrt(pi))
+    const = 1/sqrt(2**n*factorial(n)*sqrt(pi)) #Just to clean up the return call
     return const*exp(-(x**2/2))*H(n,x)
